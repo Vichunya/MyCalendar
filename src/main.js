0264128c4fece06932bdaabb0a95ad06e58a73c9
Vik <victoria.shin.ap@gmail.com>
2025-06-01 14:43:42 +0300

    1 June modal start

--- src/main.js
+++ src/main.js
@@ -1,24 +1,13 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Igor!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const modal = document.querySelector('.modal'); 
+const dates = document.querySelectorAll('.dates div');
 
-setupCounter(document.querySelector('#counter'))
+dates.forEach(date => {
+  date.addEventListener('click', () => {
+    modal.classList.add('open'); // открываем модалку
+  });
+});
+
