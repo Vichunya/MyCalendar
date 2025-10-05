f52ae4b6553ca7a091d2ec305da0f1ed4b90a6cf
Vik <victoria.shin.ap@gmail.com>
2025-10-05 14:39:05 +0300

    weather input October 5 2025

--- src/main.js
+++ src/main.js
@@ -224,3 +224,27 @@ document.addEventListener('touchend', () => {
 });
 
 
+const weatherForecast = document.querySelector('.weather');
+let url = "https://api.open-meteo.com/v1/forecast?latitude=55.7522&longitude=37.6156&daily=sunrise&hourly=temperature_2m,rain&timezone=Europe%2FMoscow&forecast_days=2";
+let response = await fetch(url);
+if (response.ok) { // если HTTP-статус в диапазоне 200-299
+  // получаем тело ответа (см. про этот метод ниже)
+  let json = await response.json(); // тело ответа сервера преобразованный в json
+  let hourly = json.hourly; // hourly-это объект (время, темпер., дожди) 
+  console.log(hourly);
+  let temperature = hourly.temperature_2m;
+
+  let time = hourly.time;
+  let rain = hourly.rain;
+  let daily = json.daily;
+  let sunrise = daily.sunrise;
+
+  let currentTime = new Date();
+  console.log(currentTime.getHours());
+  let currentHours = currentTime.getHours();
+  for (let i = 0; i < 3; i++) {
+    console.log(temperature[currentHours + i]);
+  }
+} else {
+  alert("Ошибка HTTP: " + response.status);
+}
